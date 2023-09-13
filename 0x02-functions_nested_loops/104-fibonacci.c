a81911e81ed9b3e22ade84647b2ec83c8fcec877
migdad <migdaddylast@gmail.com>
2023-09-13 06:28:29 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,58 +1,67 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
- * add_strings - Adds two strings representing numbers
- * @num1: The first number
- * @num2: The second number
- * @result: The result of the addition
- */
-void add_strings(char num1[], char num2[], char result[])
+* numLength - returns the length of string
+*
+* @num: operand number
+*
+* Return: number of digits
+*/
+
+int numLength(int num)
 {
-	int len1 = strlen(num1);
-	int len2 = strlen(num2);
-	int carry = 0;
-	int i, j;
+	int length = 0;
 
-	int max_len = (len1 > len2) ? len1 : len2;
+	if (!num)
+		return (1);
 
-	memset(result, 0, 5000);
-
-	for (i = len1 - 1, j = len2 - 1; 
-	     i >= 0 || j >= 0 || carry; 
-	     i--, j--, max_len--)
+	while (num)
 	{
-		int n1 = (i >= 0) ? num1[i] - '0' : 0;
-		int n2 = (j >= 0) ? num2[j] - '0' : 0;
-
-		int tmp = n1 + n2 + carry;
-		carry = tmp / 10;
-		result[max_len] = (tmp % 10) + '0';
+		num = num / 10;
+		length += 1;
 	}
+
+	return (length);
 }
 
 /**
- * main - Program entry point
- * Return: 0 on success
- */
+ * main - Entry point
+ *
+ * Description: prints the first 98 Fibonacci numbers
+ * starting with 1 and 2 followed by a new line
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-	char a[5000] = "1";
-	char b[5000] = "2";
-	char result[5000];
-	int i;
+	int count, initial0s;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, flo = 0, f20 = 0, sumo = 0;
 
-	printf("%s, %s", a, b);
-
-	for (i = 2; i < 98; i++)
+	for (count = 1; count <= 98; count++)
 	{
-		add_strings(a, b, result);
-		printf(", %s", result);
-		strcpy(a, b);
-		strcpy(b, result);
-	}
+		if (flo > 0)
+			printf("%lu", flo);
+		initial0s = numLength(mx) - 1 - numLength(f1);
 
-	printf("\n");
+		while (flo > 0 && initial0s > 0)
+		{
+			printf("%d", 0);
+			initial0s--;
+		}
+
+		printf("%lu", f1);
+
+		sum = (f1 + f2) % mx;
+		sumo = flo + f20 + (f1 + f2) / mx;
+		f1 = f2;
+		flo = f20;
+		f2 = sum;
+		f20 = sumo;
+
+		if (count != 98)
+			printf(", ");
+		else
+			printf("\n");
+	}
 	return (0);
 }
