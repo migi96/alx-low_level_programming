948a49c8ff304d08b54cc5d0e74451868f9abfa3
migdad <migdaddylast@gmail.com>
2023-09-12 17:18:32 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - prints the sum of all the multiples of 3 or 5 below 1024.
+ * Return: 0 if successful.
+ */
 int main(void)
 {
-    int i;
-    int sum = 0;
+	int i;
+	int sum = 0;
 
-    for (i = 0; i < 1024; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-            sum += i;
-    }
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
 
-    printf("%d\n", sum);
-    return (0);
+	printf("%d\n", sum);
+	return (0);
 }
