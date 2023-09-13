2d1276a581332ba4266aad5b8fba4a0b897ec468
migdad <migdaddylast@gmail.com>
2023-09-12 17:31:30 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,7 +7,7 @@ void add_strings(char num1[], char num2[], char result[])
     int len1 = strlen(num1);
     int len2 = strlen(num2);
     int carry = 0, i, j;
-    
+
     int max_len = (len1 > len2) ? len1 : len2;
 
     for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry; i--, j--, max_len--)
@@ -27,11 +27,11 @@ int main(void)
     char a[5000] = "1";
     char b[5000] = "2";
     char result[5000];
+    int i;
 
     printf("%s, %s", a, b);
 
-    for (int i = 2; i < 98; i++)
-    {
+    for (i = 2; i < 98; i++) {
         add_strings(a, b, result);
         printf(", %s", result);
 
