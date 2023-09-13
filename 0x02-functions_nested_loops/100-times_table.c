4a1d3a5d73d6a7b498f42948e35ebc74c1c3c0ee
migdad <migdaddylast@gmail.com>
2023-09-12 17:13:00 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,34 +1,50 @@
 #include "main.h"
-#include "main.h"
 
+/**
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The input value.
+ *
+ * Return: Nothing.
+ */
 void print_times_table(int n)
 {
-    int i, j, result;
-
-    if (n >= 0 && n <= 15)
-    {
-        for (i = 0; i <= n; i++)
-        {
-            for (j = 0; j <= n; j++)
-            {
-                result = i * j;
-                if (j != 0)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-
-                    if (result < 10)
-                        _putchar(' ');
-                    if (result < 100)
-                        _putchar(' ');
-                }
-                if (result >= 100)
-                    _putchar(result / 100 + '0');
-                if (result >= 10)
-                    _putchar((result / 10) % 10 + '0');
-                _putchar(result % 10 + '0');
-            }
-            _putchar('\n');
-        }
-    }
+	if (n >= 0 && n <= 15)
+	{
+		for (int i = 0; i <= n; i++)
+		{
+			for (int j = 0; j <= n; j++)
+			{
+				int product = i * j;
+				if (j == 0)
+				{
+					_putchar(product + '0');
+				}
+				else if (product <= 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product + '0');
+				}
+				else if (product <= 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product / 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(product / 100 + '0');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
