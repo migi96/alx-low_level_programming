5e18d8fcfe8015b4cbb9cd399e68ffec9f432bf6
migdad <migdaddylast@gmail.com>
2023-09-12 17:15:14 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,41 +8,30 @@
  */
 void print_times_table(int n)
 {
+	int i, j, result;
+
 	if (n >= 0 && n <= 15)
 	{
-		for (int i = 0; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (int j = 0; j <= n; j++)
+			for (j = 0; j <= n; j++)
 			{
-				int product = i * j;
-				if (j == 0)
-				{
-					_putchar(product + '0');
-				}
-				else if (product <= 9)
+				result = i * j;
+				if (j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product + '0');
+
+					if (result < 10)
+						_putchar(' ');
+					if (result < 100)
+						_putchar(' ');
 				}
-				else if (product <= 99)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product / 10 + '0');
-					_putchar(product % 10 + '0');
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(product / 100 + '0');
-					_putchar((product / 10) % 10 + '0');
-					_putchar(product % 10 + '0');
-				}
+				if (result >= 100)
+					_putchar(result / 100 + '0');
+				if (result >= 10)
+					_putchar((result / 10) % 10 + '0');
+				_putchar(result % 10 + '0');
 			}
 			_putchar('\n');
 		}
