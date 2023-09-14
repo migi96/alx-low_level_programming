e84a575f06d904349440927618386d52421cc0cf
migdad <migdaddylast@gmail.com>
2023-09-14 16:04:12 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper - checks if a character is uppercase.
- * @c: character to check
- * 
- * Return: 1 if c is uppercase, 0 otherwise.
- */
+* _isupper - checks if a character is uppercase.
+* @c: character to check
+* 
+* Return: 1 if c is uppercase, 0 otherwise.
+*/
 int _isupper(int c)
 {
     if (c >= 'A' && c <= 'Z')
