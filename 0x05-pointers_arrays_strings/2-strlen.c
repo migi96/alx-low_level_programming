753d64666c046088ffc538a7c3a3b7ffe2d6b7fd
migdad <migdaddylast@gmail.com>
2023-09-18 04:16:20 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,12 +8,12 @@
  */
 int _strlen(char *s)
 {
-    int length = 0;
+	int length = 0;
 
-    while (*s)
-    {
-        length++;
-        s++;
-    }
-    return (length);
+	while (*s)
+	{
+		length++;
+		s++;
+	}
+	return (length);
 }
