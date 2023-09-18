3df6832d8da66c205708c2e1b80f598a1c9b001a
migdad <migdaddylast@gmail.com>
2023-09-18 04:38:23 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,13 +2,14 @@
 
 /**
  * _puts - Prints a string followed by a new line.
- * @str: String to print.
+ * @str: String to be printed.
  */
 void _puts(char *str)
 {
-    while (*str)
-    {
-        _putchar(*str++);
-    }
-    _putchar('\n');
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
