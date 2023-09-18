882511d3032f6b78c6d03a48ffeaada01446c464
migdad <migdaddylast@gmail.com>
2023-09-18 06:56:56 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,39 +9,37 @@
  */
 int _atoi(char *s)
 {
-	int result = 0;
-	int sign = 1;
-	int started = 0; /* To check if we've started processing numbers */
-	int prev_result;  /* To store the previous result value */
+    int result = 0;
+    int sign = 1;
+    int started = 0; /* To check if we've started processing numbers */
 
-	while (*s)
-	{
-		if (*s == '-' && !started)
-			sign *= -1;
-		else if (*s >= '0' && *s <= '9')
-		{
-			started = 1;
-			prev_result = result;  /* Save the previous result */
-			result = result * 10 + (*s - '0') * sign;
+    while (*s)
+    {
+        if (*s == '-' && !started)
+            sign *= -1;
+        else if (*s >= '0' && *s <= '9')
+        {
+            started = 1;
+            result = result * 10 + (*s - '0') * sign;
 
-			/* Handle potential overflow */
-			if (sign > 0 && (result < prev_result || result < 0))
-			{
-				/* Handle positive overflow */
-				return (INT_MAX);
-			}
-			else if (sign < 0 && (result > prev_result || result > 0))
-			{
-				/* Handle negative overflow */
-				return (INT_MIN);
-			}
-		}
-		else if (started) 
-		{
-			break;
-		}
-		s++;
-	}
+            // Handle potential overflow
+            if (sign > 0 && result < 0)
+            {
+                // Handle positive overflow
+                return (INT_MAX);
+            }
+            else if (sign < 0 && result > 0)
+            {
+                // Handle negative overflow
+                return (INT_MIN);
+            }
+        }
+        else if (started)
+        {
+            break;
+        }
+        s++;
+    }
 
-	return (result);
+    return (result);
 }
