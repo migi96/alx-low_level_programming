e321ea705bfdd1d77e6411b9e0124f3cd64e1227
migdad <migdaddylast@gmail.com>
2023-09-20 14:14:58 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,12 +5,12 @@ char *_strncat(char *dest, char *src, int n)
     char *orig_dest = dest;
     int i = 0;
 
-    // move dest pointer to its end
+    /* move dest pointer to its end */
     while (*dest)
         dest++;
 
-    // copy at most n characters from src
-    while (i < n && *src)
+    /* copy at most n characters from src to dest */
+    while (*src && i < n)
     {
         *dest = *src;
         dest++;
@@ -18,8 +18,8 @@ char *_strncat(char *dest, char *src, int n)
         i++;
     }
 
-    if (i < n)
-        *dest = '\0';
+    if (i == n)
+        *dest = '\0'; /* terminate the string if we've copied n characters */
 
     return orig_dest;
 }
