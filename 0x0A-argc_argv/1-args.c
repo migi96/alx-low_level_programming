83d1313f592b3adaa0eeae35b7119e866180aa80
migdad <migdaddylast@gmail.com>
2023-10-02 16:39:18 -0700

    d

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
