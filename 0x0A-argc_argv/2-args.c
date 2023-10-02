0d2b1fd1e345f6e32db4ddbfb5f58ef67d85cd31
migdad <migdaddylast@gmail.com>
2023-10-02 16:44:26 -0700

    d

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument
+ * @argv: array pointing to arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int m;
+
+	for (i = 0; m < argc; m++)
+	{
+		printf("%s\n", argv[m]);
+	}
+
+	return (0);
+}
